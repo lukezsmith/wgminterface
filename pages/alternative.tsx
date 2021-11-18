325768d263b4e4010e0a2bde6223e16a2181a8de
= <luke@lukezsmith.com>
2021-11-18 11:08:08 +0000

    restyled page to circular holo & wgmi text rotation, gradient wagmi

---
+++ pages/alternative.tsx
@@ -0,0 +1,91 @@
+import type { NextPage } from "next";
+import Head from "next/head";
+import PropTypes from "prop-types";
+import Image from "next/image";
+import styles from "../styles/Home.module.css";
+import icon from "../public/logo.svg";
+import VanillaTilt from "vanilla-tilt";
+import React, { ReactFragment, useEffect } from "react";
+
+const Home: NextPage = () => {
+  // Get all tilt elements
+
+  useEffect(() => {
+    let tiltElement = document.getElementsByClassName("card")[0] as HTMLElement;
+
+    // add tiltChange event listener to them and change --angle value based on tilt details
+
+    holoEffect(tiltElement);
+
+    function holoEffect(tiltElement: Element) {
+      tiltElement.addEventListener("tiltChange", function (event) {
+        let angle =
+            // @ts-ignore: Unreachable code error
+          parseInt(event.detail.tiltY, 10) + parseInt(event.detail.tiltX, 10);
+              // @ts-ignore: Unreachable code error
+        tiltElement.style.setProperty("--angle", angle + "deg");
+      });
+    };
+    
+    VanillaTilt.init(tiltElement, {
+      scale: 1.1,
+      gyroscope: true,
+      speed: 800,
+      perspective: 1000,
+      glare: true,
+      gyroscopeMinAngleX:     -5,     // This is the bottom limit of the device angle on X axis, meaning that a device rotated at this angle would tilt the element as if the mouse was on the left border of the element;
+    gyroscopeMaxAngleX:   5,      // This is the top limit of the device angle on X axis, meaning that a device rotated at this angle would tilt the element as if the mouse was on the right border of the element;
+    gyroscopeMinAngleY:     -5,     // This is the bottom limit of the device angle on Y axis, meaning that a device rotated at this angle would tilt the element as if the mouse was on the top border of the element;
+    gyroscopeMaxAngleY:     5,   
+      "mouse-event-element": "main",
+    });
+
+    
+    
+  });
+
+  return (
+    <div className="bg-white">
+      <Head>
+        <title>WGMInterfaces</title>
+        <meta name="description" content="WGMInterfaces" />
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+
+      <main id="main" className="py-64  sm:py-40 md:py-32 xl:py-40 lg:mx-80  xl:mx-96 flex h-screen overflow-y-hidden">
+        <div className="mx-20 md:mx-80 xl:mx-40 " id="grid">
+          <div data-tilt-full-page-listening className="card">
+            <div className="card-content flex flex-col">
+              <img className="w-32 xl:w-40" src="logo.svg"/>
+              <img className="mx-16" src="wgmi.svg" />
+            </div>
+          </div>
+          <div className="m-auto mt-10 text-black">
+            <p className="text-2xl">wgmiOS</p>
+            <div className="relative pt-1">
+              <div className="overflow-hidden h-2 text-xs flex rounded bg-purple-200">
+                <div
+                  id="progress"
+                  className="
+                    shadow-none
+                    flex flex-col
+                    text-center
+                    whitespace-nowrap
+                    text-white
+                    justify-center
+                    bg-purple-500
+                  "
+                ></div>
+              </div>
+              <div className="text-center mt-10">
+                <p className="text-md">Coming soon....</p>
+                </div>
+            </div>
+          </div>
+        </div>
+      </main>
+    </div>
+  );
+};
+
+export default Home;
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,11 +1,7 @@
 import type { NextPage } from "next";
 import Head from "next/head";
-import PropTypes from "prop-types";
-import Image from "next/image";
-import styles from "../styles/Home.module.css";
-import icon from "../public/logo.svg";
 import VanillaTilt from "vanilla-tilt";
-import React, { ReactFragment, useEffect } from "react";
+import React, { useEffect } from "react";
 
 const Home: NextPage = () => {
   // Get all tilt elements
@@ -20,28 +16,26 @@ const Home: NextPage = () => {
     function holoEffect(tiltElement: Element) {
       tiltElement.addEventListener("tiltChange", function (event) {
         let angle =
-            // @ts-ignore: Unreachable code error
+          // @ts-ignore: Unreachable code error
           parseInt(event.detail.tiltY, 10) + parseInt(event.detail.tiltX, 10);
-              // @ts-ignore: Unreachable code error
+        // @ts-ignore: Unreachable code error
         tiltElement.style.setProperty("--angle", angle + "deg");
       });
-    };
-    
+    }
+
     VanillaTilt.init(tiltElement, {
       scale: 1.1,
       gyroscope: true,
       speed: 800,
       perspective: 1000,
       glare: true,
-      gyroscopeMinAngleX:     -5,     // This is the bottom limit of the device angle on X axis, meaning that a device rotated at this angle would tilt the element as if the mouse was on the left border of the element;
-    gyroscopeMaxAngleX:   5,      // This is the top limit of the device angle on X axis, meaning that a device rotated at this angle would tilt the element as if the mouse was on the right border of the element;
-    gyroscopeMinAngleY:     -5,     // This is the bottom limit of the device angle on Y axis, meaning that a device rotated at this angle would tilt the element as if the mouse was on the top border of the element;
-    gyroscopeMaxAngleY:     5,   
+      "max-glare": 0.1,
+      gyroscopeMinAngleX: -5, // This is the bottom limit of the device angle on X axis, meaning that a device rotated at this angle would tilt the element as if the mouse was on the left border of the element;
+      gyroscopeMaxAngleX: 5, // This is the top limit of the device angle on X axis, meaning that a device rotated at this angle would tilt the element as if the mouse was on the right border of the element;
+      gyroscopeMinAngleY: -5, // This is the bottom limit of the device angle on Y axis, meaning that a device rotated at this angle would tilt the element as if the mouse was on the top border of the element;
+      gyroscopeMaxAngleY: 5,
       "mouse-event-element": "main",
     });
-
-    
-    
   });
 
   return (
@@ -52,36 +46,36 @@ const Home: NextPage = () => {
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
-      <main id="main" className="py-64  sm:py-40 md:py-32 xl:py-40 lg:mx-80  xl:mx-96 flex h-screen overflow-y-hidden">
-        <div className="mx-20 md:mx-80 xl:mx-40 " id="grid">
-          <div data-tilt-full-page-listening className="card">
+      <main
+        id="main"
+        className="py-64  sm:py-40 md:py-32 xl:py-40 lg:mx-80  xl:mx-96 2xl:mx-96 flex h-screen overflow-hidden select-none"
+      >
+        <div className="mx-16 sm:mx-40 md:mx-72 lg:mx-64 xl:mx-44 2xl:mx-96 my-auto" id="grid">
+          <div data-tilt-full-page-listening className="card brand">
             <div className="card-content flex flex-col">
-              <img className="w-32 xl:w-40" src="logo.svg"/>
-              <img className="mx-16" src="wgmi.svg" />
-            </div>
-          </div>
-          <div className="m-auto mt-10 text-black">
-            <p className="text-2xl">wgmiOS</p>
-            <div className="relative pt-1">
-              <div className="overflow-hidden h-2 text-xs flex rounded bg-purple-200">
-                <div
-                  id="progress"
-                  className="
-                    shadow-none
-                    flex flex-col
-                    text-center
-                    whitespace-nowrap
-                    text-white
-                    justify-center
-                    bg-purple-500
-                  "
-                ></div>
+              <img className="w-32 xl:w-64" src="logo.svg" />
+              <div className="brand-label">
+                <svg className=""
+                  xmlns="http://www.w3.org/2000/svg"
+                  xmlnsXlink="http://www.w3.org/1999/xlink"
+                  viewBox="0 0 300 300"
+                  aria-hidden="true"
+                >
+                  <defs>
+                    <path id="a" d="M90 150a60 60 0 01120 0 60 60 0 01-120 0" />
+                  </defs>
+                  <use xlinkHref="#a" fill="none" />
+                  <text>
+                    <textPath xlinkHref="#a">
+                      {" "}
+                      WGMInterfaces ◕◡◕ WGMInterfaces ◕◡◕ WGMInterfaces ◕◡◕
+                    </textPath>
+                  </text>
+                </svg>
               </div>
-              <div className="text-center mt-10">
-                <p className="text-md">Coming soon....</p>
-                </div>
             </div>
           </div>
+          <img id="wagmi" className="pt-20" src="wgmi.svg" />
         </div>
       </main>
     </div>
